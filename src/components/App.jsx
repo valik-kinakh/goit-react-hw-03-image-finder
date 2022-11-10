4313090a2732dea680aed594755eb264d06d0692
Valik Kinakh <valik.kinah@gmail.com>
2022-11-10 15:55:31 +0200

    refactor code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Audio } from 'react-loader-spinner';
 import ImageGallery from './Components/ImageGallery';
 import SearchBar from './Components/Searchbar';
 import pixabayAPI from '../services/imageAPI';
@@ -9,36 +8,23 @@ import ErrorMessage from './Components/ErrorMessage';
 import Request from './Components/Request';
 import Button from './Components/Button';
 import Modal from './Components/Modal';
-
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
+import Loader from './Components/Loader';
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [images, setImages] = useState(null);
   const [page, setPage] = useState(1);
-  const [error, setError] = useState(null);
-  const [total, setTotal] = useState(null);
-  const [status, setStatus] = useState(Status.IDLE);
+  const [error, setError] = useState('');
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [largeURL, setLargeURL] = useState('');
 
   useEffect(() => {
     if (!query) return;
 
-    setStatus(Status.PENDING);
-    firstFetchImages(query, page);
-  }, [query]);
-
-  useEffect(() => {
-    if (page === 1) return;
-
-    nextFetchImages(query, page);
-  }, [page]);
+    setLoading(true);
+    getImages(query, page);
+  }, [query, page]);
 
   useEffect(() => {
     window.scrollTo({
@@ -47,24 +33,14 @@ const App = () => {
     });
   }, [images]);
 
-  const firstFetchImages = (query, page) => {
-    pixabayAPI.fetchImage(query, page).then(({ hits, total }) => {
-      setImages(hits);
-      setTotal(total);
-      setStatus(Status.RESOLVED);
-      if (!total) {
-        setError('Something went wrong! Please, change your request!');
-        setStatus(Status.REJECTED);
-      } else {
-        setError(null);
-      }
-    });
-  };
-
-  const nextFetchImages = (query, page) => {
-    pixabayAPI.fetchImage(query, page).then(({ hits }) => {
-      setImages(prevImg => [...prevImg, ...hits]);
-    });
+  const getImages = (query, page) => {
+    pixabayAPI
+      .fetchImage(query, page)
+      .then(({ hits }) => {
+        setImages(prevImg => [...prevImg, ...hits]);
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false));
   };
 
   const handleFormSubmit = query => {
@@ -77,12 +53,12 @@ const App = () => {
   };
 
   const openModal = url => {
-    setShowModal(!showModal);
+    setShowModal(true);
     setLargeURL(url);
   };
 
   const closeModal = () => {
-    setShowModal(!showModal);
+    setShowModal(false);
     setLargeURL('');
   };
 
@@ -91,15 +67,13 @@ const App = () => {
       <SearchBar onSubmit={handleFormSubmit} />
       <Section>
         <Container>
-          {status === 'idle' && <Request />}
-          {status === 'rejected' && <ErrorMessage message={error} />}
-          {status === 'resolved' && (
-            <ImageGallery images={images} openModal={openModal} />
+          {!query && <Request />}
+          {error && <ErrorMessage message={error} />}
+          {!error && <ImageGallery images={images} openModal={openModal} />}
+          {loading && <Loader />}
+          {images.length > 0 && !loading && (
+            <Button onClick={handleIncrement} />
           )}
-          {status === 'pending' && (
-            <Audio type="Watch" color="#00BFFF" height={80} width={80} />
-          )}
-          {total - page * 12 > 0 && <Button onClick={handleIncrement} />}
         </Container>
       </Section>
       {showModal && (
